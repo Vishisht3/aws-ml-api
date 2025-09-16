3d6aca291f547f7b28a4cc40711c8dcb37c5ebb4
Vishisht Magan <Vishisht.23FE10CAI00547@muj.manipal.edu>
2025-09-16 17:19:22 +0530

    Update my-stack.ts

--- deploy/lib/my-stack.ts
+++ deploy/lib/my-stack.ts
@@ -1,1 +1,18 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
 
+export class MyStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // Example: create an S3 bucket
+    new s3.Bucket(this, 'MyBucket', {
+      versioned: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+    });
+
+    // Add other AWS resources here as needed
+  }
+}
