04ee032d6f862a98ce25868c184b821dcb606e1e
Vishisht Magan <Vishisht.23FE10CAI00547@muj.manipal.edu>
2025-09-16 17:18:49 +0530

    Update app.ts

--- deploy/bin/app.ts
+++ deploy/bin/app.ts
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
 import { MyStack } from '../lib/my-stack';
-
 const app = new cdk.App();
 new MyStack(app, 'MyStack', {
   // stack props if needed
